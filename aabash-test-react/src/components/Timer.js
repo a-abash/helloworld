60b19dc9755c190779ab9ada7217382e8c54f260
a_abash <a.abashkin93@gmail.com>
2022-01-19 16:17:36 +0200

    Counts

--- aabash-test-react/src/components/Timer.js
+++ aabash-test-react/src/components/Timer.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
+const INITIAL_COUNT = 0
+
 const Timer = () => {
-  const [seconds, setSeconds] = useState(0);
+  const [hours, setHours] = useState(INITIAL_COUNT);
+  const [minutes, setMinutes] = useState(INITIAL_COUNT);
+  const [seconds, setSeconds] = useState(INITIAL_COUNT);
   const [isActive, setIsActive] = useState(false);
 
   function toggle() {
@@ -9,12 +13,16 @@ const Timer = () => {
   }
 
   function stop() {
-    setSeconds(0);
+    setHours(INITIAL_COUNT);
+    setMinutes(INITIAL_COUNT);
+    setSeconds(INITIAL_COUNT);
     setIsActive(false);
   }
 
   function reset() {
-    setSeconds(0);
+    setHours(INITIAL_COUNT);
+    setMinutes(INITIAL_COUNT);
+    setSeconds(INITIAL_COUNT);
     setIsActive(true);
   }
 
@@ -23,17 +31,30 @@ const Timer = () => {
     if (isActive) {
       interval = setInterval(() => {
         setSeconds(seconds => seconds + 1);
+
+        if (seconds === 60) {
+          setMinutes(minutes => minutes + 1);
+          
+          setSeconds(INITIAL_COUNT);
+        }
+
+        if (minutes === 60) {
+          setHours(hours => hours + 1);
+          setMinutes(INITIAL_COUNT);
+        }
       }, 1000);
-    } else if (!isActive && seconds !== 0) {
+    } else if (!isActive && seconds !== INITIAL_COUNT) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, seconds]);
+  }, [isActive, seconds, minutes, hours]);
 
   return (
     <>
       <div className="time">
-        {seconds}s
+        <span className="counter">{hours}</span>hours
+        <span className="counter">{minutes}</span>minutes
+        <span className="counter">{seconds}</span>seconds
       </div>
       <div className="row">
         <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
